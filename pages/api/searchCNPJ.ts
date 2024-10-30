a5934b8f217e32b05e7d81aef5bd957d53e14f68
rai joia <megac154@gmail.com>
2024-10-30 17:37:21 -0300

    feat: add endpoint for seacrh cnpj

---
+++ pages/api/searchCNPJ.ts
@@ -0,0 +1,28 @@
+import type { EmpresaType } from '@/interface/responseCNPJType'
+import axios from 'axios'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<EmpresaType | { error: string }>
+  // biome-ignore lint/suspicious/noConfusingVoidType: <explanation>
+): Promise<EmpresaType | void> {
+  const { cnpj } = req.query
+
+  const options = {
+    method: 'GET',
+    url: `https://brasilapi.com.br/api/cnpj/v1/${cnpj}`,
+    headers: { 'Content-Type': 'application/json' },
+  }
+
+  try {
+    const response = await axios.request(options)
+    console.log(response)
+    return res.status(200).json(response.data)
+
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  } catch (error: any) {
+    console.error('An error occurred:', error)
+    return res.status(500).json({ error: 'An internal error occurred.' })
+  }
+}
